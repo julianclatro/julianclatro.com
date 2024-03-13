85ee9746c1f90adcc37af96022e59365a9d93f91
Julian <jclatro@yahoo.com>
2024-03-13 16:06:48 -0300

    updates

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export const links: LinksFunction = () => {
 
 export default function App() {
 
-
+  const itemStyle = "hover:text-blue-500"
   return (
     <html lang="en" className="bg-slate-200 overscroll-none">
       <head>
@@ -40,7 +40,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-      <div className="sm:max-w-screen-lg max-w-screen-sm w-full mx-auto">
+        {/* <div className="sm:max-w-screen-lg max-w-screen-sm w-full mx-auto">
         <div className="bg-slate-50 border border-slate-900 rounded-[8px]">
           <div className="p-[10px] border-b border-1 rounded-t-[7px] h-[6vh]">
             <Menu />
@@ -52,7 +52,37 @@ export default function App() {
             <Footer />
           </div>
           </div>
+        </div> */}
+        <div className="sm:max-w-screen-lg max-w-screen-sm w-full mx-auto mt-10">
+          <div className="tk-forma-djr-display font-bold text-5xl">Julian Clatro</div>
+          <div>Hacker & Designer</div>
+          <div>Shipping software at <a href="https://hacksur.com" className="text-sky-600 underline">hacksur</a></div>
+          <div className="mt-10">
+            <div className="text-xl">Ongoing projects: </div>
+            <div><a href="https://nobak.net" className="text-l">NOBAK</a></div>
+            <div><a href="https://funskill.cc">Funskill</a></div>
+            <div><a href="https://filmara.org">Filmara</a></div>
+          </div>
+          <div className="mt-10">
+            <div className="text-xl">Past projects: </div>
+            <div>
+              <a href="https://datasa.info" className="text-l">datasa</a>
+              {/* <div>Datos Socioambientales</div> */}
+            </div>
+          </div>
+          <div className="mt-10">
+            <div>
+              Social Networks
+            </div>
+            <div className="flex flex-column space-x-[20px] items-end h-full">
+              <a className={itemStyle} href="https://discordapp.com/users/838749542685474816">Discord</a>
+              <a className={itemStyle} href="https://github.com/julianclatro">Github</a>
+              <a className={itemStyle} href="https://dribbble.com/julianclatro">Dribble</a>
+              {/* <a className={itemStyle} href="https://figma.com/@julianclatro">Figma</a> */}
+            </div>
+          </div>
         </div>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
